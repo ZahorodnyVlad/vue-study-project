d6dc49ce1a11c7eeb9a04bd0c400dffdf94bb285
vladZagorodnij <zvladm07@gmail.com>
2018-09-19 23:28:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-toolbar app></v-toolbar>
+    <v-content>
+
+
+        <router-view></router-view>
+
+
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
